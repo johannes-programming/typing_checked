a3467f508fff6e3b166ad93beb931e0f971c35b0
Johannes <johannes-programming@posteo.org>
2023-12-27 00:20:38 +0100

    Version 1.0.0

--- src/typing_checked/__init__.py
+++ src/typing_checked/__init__.py
@@ -1,6 +1,1 @@
-def main(args=None):
-    print("Hello World!")
-    
-if __name__ == '__main__':
-    main() 
-
+from . import easymode
--- src/typing_checked/__main__.py
+++
@@ -1,5 +0,0 @@
-from typing_checked import main
-
-if __name__ == '__main__':
-    main()
-
